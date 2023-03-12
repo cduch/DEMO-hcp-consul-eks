43f3b6d6739066c589ee7c81db508925c5dd690a
Carsten Duch <carsten@hashicorp.com>
2023-03-12 15:34:42 +0100

    added hcp vars

--- variables.tf
+++ variables.tf
@@ -3,6 +3,17 @@ variable "region" {
   default = "eu-central-1"
 }
 
+variable "hcp_client_id" {
+  description = "The HCP Client ID"
+  type        = string
+  sensitive   = false
+}
+
+variable "hcp_client_secret" {
+  description = "The HCP Client Secret (sensitive)"
+  type        = string
+  sensitive   = true
+}
 
 variable "cluster_id" {
   type    = string
